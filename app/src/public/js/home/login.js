2e8bd6863afd3312c2ccf6d10ae21d5ab9f95a41
Choi ho jin <jkhh9725@gmail.com>
2023-06-22 16:50:05 +0900

    DOM을 이용한 로그인 값 가져오기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,6 +1,18 @@
 "use script";
 
-console.log("굿");
-console.log("바이");
-console.log("바이");
-console.log("이젠 보지 말자");
+// DOM -> Document Object Model 을 통해 제어한다.
+
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+console.log(id, psword, loginBtn);
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
