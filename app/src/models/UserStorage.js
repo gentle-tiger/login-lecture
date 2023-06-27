3723586e9b8cb2484a689d8674f54d57c7d8b7cf
Choi ho jin <jkhh9725@gmail.com>
2023-06-27 17:15:10 +0900

    유저 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,15 +3,14 @@
 // 데이터의 은닉화,   getUsers라는 새로운 메서드로 users 데이터를 전달해주어야 한다.
 class UserStorage {
   static #users = {
-    // #을 붙여서 private 변수로 저장해야한다. 일단
-    id: ["hojin0101", "이친구", "저친구"],
+    id: ["hojin1234", "이친구", "저친구"], // 이게 하나의 필드인 거임.
     psword: ["1234", "1234", "123456"],
     name: ["최호진", "이친구", "저친구"],
   };
 
   static getUsers(...fields) {
     // ...fields에 들어오는 파라미터는 이 UserStorage.getUsers 를 사용하는 곳에 적혀있다.
-    const users = this.#users;
+    const users = this.#users; // 일반적인 참조 복사
     const newUsers = fields.reduce((newUsers, fields) => {
       if (users.hasOwnProperty(fields)) {
         newUsers[fields] = users[fields];
@@ -19,6 +18,18 @@ class UserStorage {
       return newUsers;
     }, {}); // 이거 잘 이해해보자.
     return newUsers;
+  }
+
+  // User.js에서 던져주는 유저의 id 값을 아이디에 해당하는 데이터를 object로 전달하는 메서드를 만든다.
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // users를 받아와서 이 users의 키 값들만 리스트로 만든다. =>[id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx]; // users의 키값의 idx를 구한다. 는 id값이다.
+      return newUser;
+    }, {});
+    return userInfo;
   }
 }
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,8 +1,8 @@
 "use strict";
+const User = require("../../models/User");
 // 이 함수들은 단순히 해당 페이지를 렌더링 해주는 함수이다.
 
 // home.ctrl.js 파일은 정의해 놓ㅇ느 객체를 내보내는 모듈로, 해당 모듈을 렌더링하는 함수들과 로그인 처리를 수행하는 함수를 정의한다.
-const UserStorage = require("../../models/UserStorage"); /// 상위 상위 폴더의 모델 폴더 ...
 const output = {
   // home 함수는 res.render("home/index")를 호출하여 "home/index"라는 템플릿을 렌더링합니다.
   home: (req, res) => {
@@ -16,24 +16,12 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      psword = req.body.psword;
-    // class 자체에서 바로 접근하기 위해서는 UserStorage에  static을 붙여서 정적 변수로 만들어 주어야 한다.
-    // 그런데,! 실제 상용화되는 곳에서는 이러한 식으로 바로 접근할 수 없어야 한다. 그래서 은닉화가 필요하다.
-    const users = UserStorage.getUsers("id", "psword"); //
-
-    const response = {};
-    if (user.id.includes(id)) {
-      const idx = user.id.indexOf(id);
-      if (user.psword[idx] === psword) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = "로그인에 실패하였습니다.";
-    return res.json(response);
+    // User가 기본적으로  가지고 있도록 한다. User라는 클래스를  req.body를 넣어서 인스턴스화 한다.
+    // user는 body를 항상 들고 다니게 된다.
+    const user = new User(req.body);
+    const response = user.login();
+    console.log(response);
+    // return res.json(response); //json의 형태로 가져온다
   },
 };
 
